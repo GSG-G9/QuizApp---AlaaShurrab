817f3a7cc29a253d648a8c4259c83a85858ef14c
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2020-10-20 18:20:32 +0300

    added the questionArray again

---
+++ js.js
@@ -0,0 +1,162 @@
+let questionArray = [
+    {
+        questionNo : "1",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "2",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "3",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "4",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "5",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "6",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "7",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "8",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "9",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "10",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "11",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "12",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "13",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "14",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "15",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "16",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "17",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "18",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "19",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    },
+    {
+        questionNo : "20",
+        answer1 : "aaaaaaaaaa",
+        answer2 : "bbbbbbbbbb",
+        answer3 : "cccccccccc",
+        answer4 : "dddddddddd",
+        rghitAnswer : "aaaaaaaaaa"
+    }
+];
